de8b06e49f79e34be19a197aec2442f255f670d7
juanarangoceo <juanarangopm@gmail.com>
2026-01-23 00:21:21 -0500

    fix: update eslint.config.mjs to ignore specific rules and use next lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,17 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "test-image-gen.js",
+    "*.js"
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "import/no-anonymous-default-export": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
